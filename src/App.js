d10e03e95a75a0e5073b0e4631d152490ba87ba3
bj9 <bj9@illinois.com>
2021-02-22 21:18:36 -0500

    added empty SVG

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {Visualization} from './components/Visualization';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Visualization />
         <p>
           Landing page for Design 1
         </p>
---
+++ src/components/Visualization.js
@@ -0,0 +1,5 @@
+export const Visualization = () => {
+    const width = 2048;
+    const height = 1024;
+    return <svg width={width} height={height} />;
+  };
